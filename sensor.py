673a82f6982418cc325e7584862b5b92f05b9c06
Max Parker <prkrmx@gmail.com>
2020-08-10 09:47:22 +0100

    Sensor ctrl

--- sensor.py
+++ sensor.py
@@ -9,21 +9,20 @@ pi = pigpio.pi()
 if not pi.connected:
     exit(0)
 
-sensor = pi.spi_open(0, 900000, 3)
+sensor = pi.spi_open(0, 1000000, 1)
 while True:
     try:
         c, d = pi.spi_read(sensor, 2)
         if c == 2:
             word = (d[0] << 8) | d[1]
-            t = (word >> 5)
-            if (word & 0x18) == 0:
-                # t = (word >> 5)/2.8495
+            t = (word >> 5)/2.8495
+            if (word & 0x38) == 0x20:
                 print("{:b}\t{:.2f}".format(word, t))
             else:
                 print("{:b}\t{:.2f} BAD READING!!!!".format(word, t))
 
         # c, d = pi.spi_read(sensor, 1)
-        # c, dd = pi.spi_read(sensor, 1)
+        # c, dd = pi.spi_read(sensor, 1)    
         # if c == 1:
         #     word = (d[0] << 8) | dd[0]
         #     t = (word >> 5)
